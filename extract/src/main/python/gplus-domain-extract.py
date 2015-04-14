4923f5dbda7f3f1da469a75471f0ba7913cf996c
Gilmar Souza <gilmarj@ciandt.com>
2015-04-14 13:17:25 -0300

    Read posts first try

--- extract/src/main/python/gplus-domain-extract.py
+++ extract/src/main/python/gplus-domain-extract.py
@@ -29,13 +29,31 @@ def authenticate():
     # Setting the sub field with USER_EMAIL allows you to make API calls using the
     # special keyword 'me' in place of a user id for that user.
     credentials = SignedJwtAssertionCredentials(SERVICE_ACCOUNT_EMAIL, key,
-    scope=SCOPES, sub=USER_EMAIL)
+            scope=SCOPES, sub=USER_EMAIL)
     http = httplib2.Http()
     http = credentials.authorize(http)
 
     # Create and return the authorized API client
     return build('plusDomains', 'v1', http=http)
 
+def read_posts(user_id):
+    service = authenticate()
+    activities_service = service.activities()
+    request = activities_service.list(
+            userId=user_id,
+            collection='user',
+            maxResults='2')
+
+    while request is not None:
+        activities_document = request.execute()
+        if activities_document.get('items') is not None:
+            print 'got page with %d' %
+            len(activities_document.get('items'))
+            for activity in activities_document.get('items'):
+                print activity.get('id'),activity.get('object').get('content')
+
+    request = activities_service.list_next(request, activities_document)
+
 if __name__ == '__main__':
-    authenticate()
+    read_posts('me') 
 
